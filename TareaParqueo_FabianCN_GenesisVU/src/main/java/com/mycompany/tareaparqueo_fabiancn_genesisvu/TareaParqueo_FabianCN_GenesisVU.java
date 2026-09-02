2f24e7accedc4753be845432b092da5bc93e240a
fcncr <fabicambro1@gmail.com>
2026-09-02 13:51:31 -0600

    Creación de proyecto
    Creamos el proyecto principal

---
+++ TareaParqueo_FabianCN_GenesisVU/src/main/java/com/mycompany/tareaparqueo_fabiancn_genesisvu/TareaParqueo_FabianCN_GenesisVU.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tareaparqueo_fabiancn_genesisvu;
+
+/**
+ *
+ * @author fabic
+ */
+public class TareaParqueo_FabianCN_GenesisVU {
+
+    public static void main(String[] args) {
+        
+    }
+}
