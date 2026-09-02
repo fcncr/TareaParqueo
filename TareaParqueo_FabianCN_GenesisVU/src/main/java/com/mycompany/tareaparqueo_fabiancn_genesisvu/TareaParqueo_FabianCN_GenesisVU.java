e327fb82d53ab8fad37690b5d734cbfe28699dc4
fcncr <fabicambro1@gmail.com>
2026-09-02 13:57:03 -0600

    Update TareaParqueo_FabianCN_GenesisVU.java

--- TareaParqueo_FabianCN_GenesisVU/src/main/java/com/mycompany/tareaparqueo_fabiancn_genesisvu/TareaParqueo_FabianCN_GenesisVU.java
+++ TareaParqueo_FabianCN_GenesisVU/src/main/java/com/mycompany/tareaparqueo_fabiancn_genesisvu/TareaParqueo_FabianCN_GenesisVU.java
@@ -11,6 +11,7 @@ package com.mycompany.tareaparqueo_fabiancn_genesisvu;
 public class TareaParqueo_FabianCN_GenesisVU {
 
     public static void main(String[] args) {
-        
+        System.out.println("Hola");
+
     }
 }
