c6d75325123c6a7fbfd7ff5cd3747e690351ed30
fcncr <fabicambro1@gmail.com>
2026-09-04 00:50:55 -0600

    Create Vehiculo.java

---
+++ TareaParqueo/src/main/java/com/mycompany/tareaparqueo/Vehiculo.java
@@ -0,0 +1,141 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tareaparqueo;
+
+/**
+ *
+ * @author fabic
+ */
+public class Vehiculo {
+    private String tipo;
+    private String placa;
+    private String descripcion;
+    private boolean enParqueo;
+    private int cantidadEspacios;
+    private Movimiento[] movimientos;
+    private int cantidadMovimientos;
+
+    //Constructor
+
+    public Vehiculo(String tipo, String placa, boolean enParqueo, int cantidadEspacios) {
+        this.tipo = tipo;
+        this.placa = placa;
+        this.enParqueo = enParqueo;
+        this.cantidadEspacios = cantidadEspacios;
+        
+     
+        this.enParqueo = false;
+
+        movimientos = new Movimiento[100];
+        cantidadMovimientos = 0;
+    }
+    
+    public Vehiculo(String tipo, boolean enParqueo, int cantidadEspacios, String descripcion) {
+        this.tipo = tipo;
+        this.enParqueo = enParqueo;
+        this.cantidadEspacios = cantidadEspacios;
+        this.descripcion = descripcion;
+        
+        this.enParqueo = false;
+
+        movimientos = new Movimiento[100];
+        cantidadMovimientos = 0;
+    }
+    
+    //TO STRING
+    /*
+    @Override
+    public String toString() {
+        
+        String texto = "";
+        
+        texto = "Vehiculo tipo: " + tipo + "\n"+
+                "Placa: " + placa +"\n"+
+                "Descripcion" + descripcion + "\n";
+                
+        return texto;
+    }
+    */
+    
+    //GETTERS
+    
+     public String getTipo() {
+        return tipo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public boolean isEnParqueo() {
+        return enParqueo;
+    }
+
+    public int getCantidadEspacios() {
+        return cantidadEspacios;
+    }
+
+    public Movimiento[] getMovimientos() {
+        return movimientos;
+    }
+
+    public int getCantidadMovimientos() {
+        return cantidadMovimientos;
+    }
+    
+    //SETTERS
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setEnParqueo(boolean enParqueo) {
+        this.enParqueo = enParqueo;
+    }
+
+    public void setCantidadEspacios(int cantidadEspacios) {
+        this.cantidadEspacios = cantidadEspacios;
+    }
+
+    public void setMovimientos(Movimiento[] movimientos) {
+        this.movimientos = movimientos;
+    }
+
+    public void setCantidadMovimientos(int cantidadMovimientos) {
+        this.cantidadMovimientos = cantidadMovimientos;
+    }
+    
+    
+    
+    public void agregarMovimiento(Movimiento movimiento){
+        
+        if (this.cantidadMovimientos < movimientos.length){
+            this.movimientos[this.cantidadMovimientos] = movimiento;
+            this.cantidadMovimientos++;
+        }
+ 
+    }
+    
+    public void cambiarEstado(){
+        if (this.enParqueo){
+                this.enParqueo = false;
+            }
+            else
+                this.enParqueo = true;
+    }
+   
+}
