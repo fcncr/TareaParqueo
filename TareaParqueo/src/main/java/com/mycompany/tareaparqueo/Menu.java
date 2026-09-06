f12726b607bef375cae63db429e14c8073dd3093
fcncr <fabicambro1@gmail.com>
2026-09-06 00:56:12 -0600

    Menu

--- TareaParqueo/src/main/java/com/mycompany/tareaparqueo/Menu.java
+++ TareaParqueo/src/main/java/com/mycompany/tareaparqueo/Menu.java
@@ -1,17 +1,15 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.tareaparqueo;
 
 /**
  *
  * @author fabic
  */
-public class TareaParqueo {
-
-    public static void main(String[] args) {
-        Menu menu = new Menu();
-        menu.ejecutar();
+public class Menu {
+    public void ejecutar(){
+        return; 
     }
 }
